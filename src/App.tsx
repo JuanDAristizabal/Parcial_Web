22747a901a049d20ecdb97e80ea9078ed31bb2ec
JUAN-JOSE-CADENA <69523656+JUAN-JOSE-CADENA@users.noreply.github.com>
2022-04-07 16:03:55 -0500

    titulo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <div className='Comtainer'>
-        
+        <h1><b>AHORCADO</b></h1>
+        <h3></h3>
       </div>
     </div>
   );
