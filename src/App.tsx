05d7b8d9b2c6ed9eab67cb1c5ba10942f3590468
JUAN-JOSE-CADENA <69523656+JUAN-JOSE-CADENA@users.noreply.github.com>
2022-04-07 16:46:07 -0500

    nuevos div

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className='Comtainer'>
+      <div className='container'>
         <div className="texto">
         <h1><b>AHORCADO</b></h1>
         <h3>Encuentra la palabra - Ingrese una letra</h3>
         </div>
+        <div className="medio">
         <div className="horca">
           <div className="cuerpo">
            <b className="cabeza">O</b>
@@ -21,6 +22,7 @@ function App() {
         </div>
         <div className="palabra">
         dfsfds
+        </div>
         </div>
         <div className="letras">
           dfs
