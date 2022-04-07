a67ca301ce282f717271f0b5251c8bc91b748028
JUAN-JOSE-CADENA <69523656+JUAN-JOSE-CADENA@users.noreply.github.com>
2022-04-07 16:09:36 -0500

    texto div

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,13 @@ function App() {
   return (
     <div className="App">
       <div className='Comtainer'>
+        <div className="texto">
         <h1><b>AHORCADO</b></h1>
-        <h3></h3>
+        <h3>Encuentra la palabra - Ingrese una letra</h3>
+        </div>
+        <div className="horca">
+          
+        </div>
       </div>
     </div>
   );
